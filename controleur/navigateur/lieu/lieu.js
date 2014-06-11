9bc302e46122dc00af38148e7dd0c9b224e24b6b
Chopintropbien <mollierlauriane@gmail.com>
2014-06-11 11:21:27 +0200

    modified:   .idea/workspace.xml 	modified:   controleur/navigateur/lieu/lieu.js 	modified:   controleur/navigateur/lieu/lieu.php 	modified:   vue/navigateur/navigateur.css

--- controleur/navigateur/lieu/lieu.js
+++ controleur/navigateur/lieu/lieu.js
@@ -2,20 +2,117 @@
 (function() {
 
     var lieuRecherche = document.getElementById('ou');
+    var resultatRecherche = document.getElementById('ajaxOu');
+    var selectedResult = -1;
+    var selectedClass = 'selected';
+    var ancienneLettreTappe;
+    var previousRequest;
 
-    addEvent(lieuRecherche, 'keyup', function(e){
+    function getResults(text){
         var xhr = new XMLHttpRequest();
         xhr.open('POST', 'http://localhost/meittopi/controleur/navigateur/lieu/lieu.php');
-        xhr.send('lieu=' + this.value);
-
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function() { // On gère ici une requête asynchrone
-
-            if (xhr.readyState == 4 && xhr.status == 200) { // Si le fichier est chargé sans erreur
-                alert(xhr.responseText);
+            if (xhr.readyState == 4) { // TODO: ajouter  && xhr.status == 200
+                displayResults(xhr.responseText);
             }
         };
+        xhr.send('lieu=' + text);
+        return xhr;
+    }
 
-        xhr.send(null);
+    function displayResults(response){
+
+        resultatRecherche.style.display = response.length ? 'block' : 'none';
+
+        if(response.length){
+            resultatRecherche.innerHTML = '';
+
+            var responseTableau = response.split('|');
+            for(var i = 0; i < responseTableau.length; ++i){
+                var li = resultatRecherche.appendChild(document.createElement('li'));
+                li.innerHTML = responseTableau[i];
+
+                addEvent(li, 'click', function(){
+                    choiceResult(this);
+                });
+            }
+        }
+    }
+
+    function choiceResult(choice){
+        lieuRecherche.value = choice.innerHTML;
+        resultatRecherche.innerHTML = '';
+        resultatRecherche.style.display = 'none';
+        selectedResult = -1;
+        lancerRecherche();
+    }
+
+    function lancerRecherche(){
+        //TODO: ne pas faire semblant
+        alert('la requette fait semblais d etre lancer');
+    }
+
+    addEvent(lieuRecherche, 'keyup', function(e){
+
+        var liProposition = resultatRecherche.childNodes;
+
+        if(e.keyCode == 38){ // pour la touche haut
+
+            if(selectedResult == 0){
+                liProposition[selectedResult].className = '';
+                selectedResult = -1;
+                lieuRecherche.value = ancienneLettreTappe;
+            }
+            else if (selectedResult == -1){
+                selectedResult = liProposition.length -1;
+                liProposition[selectedResult].className = selectedClass;
+                lieuRecherche.value = liProposition[selectedResult].innerHTML;
+            }
+            else{
+                liProposition[selectedResult].className = '';
+                selectedResult = selectedResult -1;
+                liProposition[selectedResult].className = selectedClass;
+                lieuRecherche.value = liProposition[selectedResult].innerHTML;
+            }
+        }
+
+        else if(e.keyCode == 40){ // pour la touche bas
+
+            if(selectedResult == liProposition.length-1){
+                liProposition[selectedResult].className = '';
+                selectedResult = -1;
+                lieuRecherche.value = ancienneLettreTappe;
+            }
+            else if (selectedResult == -1){
+                selectedResult = selectedResult +1;
+                liProposition[selectedResult].className = selectedClass;
+                lieuRecherche.value = liProposition[selectedResult].innerHTML;
+            }
+            else{
+                liProposition[selectedResult].className = '';
+                selectedResult = selectedResult +1;
+                liProposition[selectedResult].className = selectedClass;
+                lieuRecherche.value = liProposition[selectedResult].innerHTML;
+            }
+        }
+
+        else if(e.keyCode == 13){ // pour la touche entree
+            if(selectedResult != -1) choiceResult(liProposition[selectedResult]);
+            else ; lancerRecherche();
+        }
+
+        else if(ancienneLettreTappe != this.value){
+            ancienneLettreTappe = this.value;
+            if (previousRequest && previousRequest.readyState < 4) {
+                previousRequest.abort(); // Si on a toujours une requête en cours, on l'arrête
+            }
+            previousRequest = getResults(this.value);
+            selectedResult = -1;
+        }
+
+
+
 
     });
 
